876e77bd33238f44a517a33f2f38d64a8bfe9573
Tony Bowden <tony@tmtm.com>
2016-02-26 21:59:56 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,31 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-require 'rest-client'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
-end
-
-def wikinames(url)
-  noko = noko_for(url)
-  noko.xpath('//h2[span[@id="Members"]]/following-sibling::table[1]//tr[td]/td[2]//a[not(@class="new")]/@title').map(&:text)
-end
-
-names = wikinames('https://en.wikipedia.org/wiki/9th_Parliament_of_Solomon_Islands')
-
-WikiData.ids_from_pages('en', names).each_with_index do |p, i|
-  data = WikiData::Fetcher.new(id: p.last).data('fr') rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  ScraperWiki.save_sqlite([:id], data)
-end
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
-
+names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/9th_Parliament_of_Solomon_Islands',
+  xpath: '//h2[span[@id="Members"]]/following-sibling::table[1]//tr[td]/td[2]//a[not(@class="new")]/@title',
+) 
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
