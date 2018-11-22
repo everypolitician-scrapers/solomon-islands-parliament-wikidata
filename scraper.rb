649ae634efa9f48fc4a3195c7b6696802f8dba97
Tony Bowden <tony@tmtm.com>
2018-11-22 14:35:21 +0000

    Also find via SPARQL

--- scraper.rb
+++ scraper.rb
@@ -3,8 +3,19 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/9th_Parliament_of_Solomon_Islands',
   xpath: '//h2[span[@id="Members"]]/following-sibling::table[1]//tr[td]/td[2]//a[not(@class="new")]/@title',
-) 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+  as_ids: true,
+)
+
+query = <<SPARQL
+  SELECT DISTINCT ?item WHERE {
+    ?item p:P39 [ ps:P39 wd:Q17633943 ; pq:P2937 ?term ] .
+    ?term p:P31/pq:P1545 ?ordinal .
+    FILTER (xsd:integer(?ordinal) >= 10)
+  }
+SPARQL
+p39s = EveryPolitician::Wikidata.sparql(query)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s | names)
